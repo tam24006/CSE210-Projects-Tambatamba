e9f4d4f145063f30faa05b0c310ded4aed7c5506
tam24006 <tam24006@byui.edu>
2026-01-20 09:06:03 -0700

    pre 1 homework

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Console.WriteLine("Hello Prep1 World!");
+        // Console.Write("There will not be a newline after this.");
+        // Console.Write("what is your Favorite color?");
+        // string color = Console.ReadLine();
+        // string school = "BYU-Idaho";
+        // Console.WriteLine($"I am studing at {school}");
+
+        Console.WriteLine("what is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.WriteLine("what is your last name?");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName}, {lastName}");
+ 
+
     }
+    
+    
+    
+        
+    
 }
+
+
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.WriteLine("Enter number:");
+        string valuefromUser = Console.ReadLine();
+
+        int x = 5;
+        int y = 2;
+
+        if (x > y)
+        {
+            Console.WriteLine("Greater");
+        }
+        else if (x < y) ;
+        {
+            Console.WriteLine("Less");
+        }
+        else
+        {
+            Console.WriteLine("Equal");
+        }
+        
+        
+
+
+
+
     }
+
+    
 }
